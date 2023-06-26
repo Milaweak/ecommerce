e4ad13be24bd3b4ee8df0d320923e26ce41c3b17
Milaweak <milaweak@gmail.com>
2023-06-26 15:09:11 +0200

    starting the project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {ProductContext} from '../contexts/ProductContext';
 
 const Home = () => {
-  return <div>Homepage</div>;
+  const {products} = useContext(ProductContext)
+  const filteredProducts = products.filter(item => {
+    return (
+     item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+
+  return <div>
+    <section className='py-16'>
+      <div className='container mx-auto'>
+        <div className='grid'>
+          {filteredProducts.map(product => {
+            return <div className='w-full h-[300px] bg-pink-200 mb-4' key={product.id}>{product.title}</div>
+          })}
+        </div>
+
+      </div>
+    </section>
+
+
+
+  </div>;
 };
 
 export default Home;
